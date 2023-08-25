faaa8eab5a69412d59bb03d1bdafb2a41296447d
Marucha <marucha.webdev@yahoo.com>
2023-08-25 19:43:26 +0300

    feat: Error Route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { HomePage } from '../Pages/Home/HomePage'
+import { Lost } from '../Pages/Lost/404'
 
 export const Paths = () => {
 	return (
 			<Routes>
 				<Route exact path='/' element={<HomePage />}/>
+				<Route path="*" element={<Lost />} />
 			</Routes>
 	)
 }
