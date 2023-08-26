9ba7d5c4b640cf603c6eade150deef37d2c7b25f
Marucha <marucha.webdev@yahoo.com>
2023-08-26 18:48:55 +0300

    feat: Single Page Route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import { HomePage } from '../Pages/Home/HomePage'
 import { Lost } from '../Pages/Lost/404'
+import { SingleCountry } from '../Components/CountryInfo/SingleCountry'
 
 export const Paths = () => {
 	return (
 			<Routes>
 				<Route exact path='/' element={<HomePage />}/>
+				<Route path="/country/:id" element={<SingleCountry />}/>
 				<Route path="*" element={<Lost />} />
 			</Routes>
 	)
