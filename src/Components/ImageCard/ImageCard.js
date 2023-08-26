2be7e1ac5b9f2136b97cfcb1ab38e0d27303f72c
Marucha <marucha.webdev@yahoo.com>
2023-08-26 20:54:35 +0300

    fix: SPlit Function was applied on wrong field

--- src/Components/ImageCard/ImageCard.js
+++ src/Components/ImageCard/ImageCard.js
@@ -12,7 +12,7 @@ export const ImageCard = ({ flag, name, population, region, capital, id }) => {
 					<h3 className="font-extrabold text-ellipsis text-xl leading-loose tracking-wider">{name}</h3>
 					<article className="font-regular flex flex-col gap-2 space-y-2">
 						<HeadingWithSpan descriptor={"Population"} info={SplitNumber(population)} />
-						<HeadingWithSpan descriptor={"Region"} info={SplitNumber(region)} />
+						<HeadingWithSpan descriptor={"Region"} info={region} />
 						<HeadingWithSpan descriptor={"Capital"} info={capital} />
 				</article>
 				</section>
