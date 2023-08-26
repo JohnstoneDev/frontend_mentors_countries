f20600c0e9c451832ddb04671463f7179b58df11
Marucha <marucha.webdev@yahoo.com>
2023-08-26 22:24:08 +0300

    style: Responsive Styling for the home page

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -7,7 +7,8 @@ export const HomePage = () => {
 	return (
 		<div className="space-y-4">
 			<div
-				className="desktop:flex desktop:flex-row mobile:flex mobile:flex-col desktop:items-center justify-between p-2.5 m-2.5">
+				className="desktop:flex desktop:flex-row mobile:flex mobile:flex-col desktop:items-center justify-between desktop:p-2.5 desktop:m-2.5
+				mobile:p-4 mobile:m-6">
 				<SearchBar />
 				<FilterDropDown />
 			</div>
